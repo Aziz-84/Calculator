c79ac9a1af269bde00552a76890ebb4428e04db0
Aziz-84 <azca0001@gmail.com>
2022-11-27 19:50:37 +0100

    check for dot functtion

--- script.js
+++ script.js
@@ -14,6 +14,20 @@ let result="null";
 let lastInput="";
 let haveDot=false;
 
+//we first check if user have dot in innerHtml,on btn pressed if not then we set
+//haveDot to true and add dot or other number.
+numbersEl.forEach(number=>{
+  number.addEventListener("click",(e)=>{
+    if(e.target.innerText==="." && !haveDot){
+       haveDot=true
+    }else if (e.target.innerText==="."&& haveDot){
+      return;//return nothing if there is a dot
+    }
+    dis2Num+=e.target.innerText;
+    display2El.innerText=dis2Num
+  })
+});
+
 function operator(){
 
   switch (lastInput) {
