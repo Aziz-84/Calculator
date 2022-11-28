c4c619c4e9a1c846ffd3e2bd03a0d2091a46f27e
Aziz-84 <azca0001@gmail.com>
2022-11-28 17:15:16 +0100

    functinal calcultor

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const clearEl = document.querySelector(".AC");
+const clearEl = document.querySelector(".C");
 const ClearLastEl= document.querySelector(".CE");
 const display1El=document.querySelector(".display-1")
 const display2El=document.querySelector(".display-2")
 const tempResultEl=document.querySelector(".temp-result")
 const numbersEl=document.querySelectorAll(".number");
 const operationEl=document.querySelectorAll(".operation");
-const equal = document.querySelector(".equal");
+const equalEL = document.querySelector(".equal");
 
 
 let dis1num="";
@@ -13,6 +13,7 @@ let dis2Num="";
 let result="null";
 let lastInput="";
 let haveDot=false;
+
 
 //we first check if user have dot in innerHtml,on btn pressed if not then we set
 //haveDot to true and add dot or other number.
@@ -27,7 +28,30 @@ numbersEl.forEach(number=>{
     display2El.innerText=dis2Num
   })
 });
-
+operationEl.forEach(operation=>{
+  operation.addEventListener("click",(e)=>{
+   if(!dis2Num)return;
+   haveDot=false;
+   const operationName=e.target.innerText;
+   if(dis1num&&dis2Num&&lastOperation){
+    operator()
+   }else{
+     result=parseFloat(dis2Num)
+   }
+   clearVar(operationName)
+   lastOperation=operationName;
+  })
+ })
+//clear function
+function clearVar(name=""){
+  dis1num+=dis2Num+ " " + name +" ";  //we are putting result 
+  //from dis2Num to dis1num varible
+  display1El.innerHTML=dis1num; //then i are showing result in first display
+  display2El.innerText=""; // we clear the display 2
+  dis2Num=""; // clear dis2Num varible
+  tempResultEl.innerHTML=result  //we arw storing result in tempdisplay
+  }
+  
 function operator(){
 
   switch (lastInput) {
@@ -48,3 +72,28 @@ function operator(){
 
 }
 
+equalEL.addEventListener("click",()=>{
+  if(!dis1num||!dis2Num)return;
+  haveDot=false;
+  operator();
+  clearVar();
+  display2El.innerText=result;
+  tempResultEl.innerText="";
+  dis2Num=result;
+  dis1num="";
+})
+
+clearEl.addEventListener("click",()=>{
+  display1El.innerHTML="";
+  display2El.innerHTML="0";
+  dis1num="";
+  dis2Num="";
+  result="";
+  tempResultEl.innerHTML="";
+})
+
+ClearLastEl.addEventListener("click",()=>{
+  display2El.innerHTML="0";
+  dis2Num="";
+
+})
